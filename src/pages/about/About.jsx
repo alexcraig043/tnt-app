2cabef789a7f3f095af26464b7bb146bf7eb3dec
machadden <maclean.h.hadden.25@dartmouth.edu>
2022-08-18 17:08:19 -0400

    rough home page and about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,7 +8,12 @@ const About = () => {
         title="Tiborones n Tortugas"
         subTitle="Protecting Costa Rica's marine resources through research and conservation"
         pageCount={1.5}
-        content={"ABOUT"}
+        content={
+          <div className="aboutContainer">
+            <div className="header">Conservation, Education, Connection.</div>
+            <div className="info">Tiburones n Tortugas (TnT) is a nonprofit based out of San Jose, Costa Rica associated with an America-based 501c3 nonprofit. We strive to help protect Costa Rica's marine treasures, with a particular emphasis on turtles and sharks, which are particularly at risk for poaching in this part of the world. We were founded by world-renowned conservationist Randall Arauz with the simple mission to protect these great species while increasing student involvement with the natural world. We offer student opportunities to come down to our research station near Monteverde, Costa Rica to perform field research, write theses, and assist with conservation work.</div>
+          </div>
+        }
       />
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,31 @@ const Home = () => {
         title="Tiborones n Tortugas"
         subTitle="Protecting Costa Rica's marine resources through research and conservation"
         pageCount={1.5}
-        content={"HOME"}
+        content={
+          <div className="homeContainer">
+            <div className="header">
+              <h1>We work in the field to preserve Costa Rica's biodiversity.</h1>
+            </div>
+            <div className="stats">
+              <div className="item">
+                <div className="stat">Scientists</div>
+                <div className="number">10</div>
+              </div>
+              <div className="item">
+                <div className="stat">Terrestrial acres</div>
+                <div className="number">10,463</div>
+              </div>
+              <div className="item">
+                <div className="stat">Marine Acres</div>
+                <div className="number">87,345</div>
+              </div>
+            </div>
+            <div className="footer">At Tiburones n Tortugas, we're a nonprofit in the business of protecting one of the most biodiverse places on Earth.</div>
+          </div>
+
+
+
+        }
       />
     </div>
   );
